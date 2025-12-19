28bd0966a5ebc5046b041abb53cd98489b01c8cf
John <tlwt2003@gmail.com>
2025-12-19 15:23:34 +0700

    original working file

---
+++ my-react-01/src/components/Greetings.jsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+function Greetings({ message }) {
+  return (
+    <div>
+       <h1>Hello, React!</h1> <p>{message}</p>{" "}
+    </div>
+  );
+}
+
+export default Greetings;
---
+++ my-react-01/src/components/User.jsx
@@ -0,0 +1,5 @@
+function User({ name }) {
+  return <p className="user-name">{name}</p>;
+}
+
+export default User;
