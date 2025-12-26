39673dc8aba420234095f6622a614cdc7d86a97c
John <tlwt2003@gmail.com>
2025-12-26 14:27:22 +0700

    react-01

--- my-react-01/src/App.jsx
+++ my-react-01/src/App.jsx
@@ -2,12 +2,22 @@ import "./App.css";
 
 import Greetings from "./components/Greetings";
 import User from "./components/User";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="card">
-       <Greetings message="" />{" "}
-       <User name={"THIT LWIN WIN THANT\n6540122"} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Greetings message="" />
+              <User name={"THIT LWIN WIN THANT\n6540122"} />
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
 }
